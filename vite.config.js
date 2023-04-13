61a37854d444b91ed3b97e84246f74071ddbb03f
KIM JOONG IL <zhenos@hotmail.com>
2023-04-13 17:50:49 +0900

    vite 빌드와 깃헙 페이지 이슈 해결

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/snu/',
   build: {
     // 프로젝트가 호스팅되는 GitHub Pages의 저장소 이름으로 경로를 설정합니다.
     // 예를 들어, 저장소 이름이 'my-project'라면 다음과 같이 설정합니다.
-    base: "/snu/",
     outDir: 'docs',
     manifest: true, // Enable the generation of the manifest file
     rollupOptions: {
@@ -14,6 +14,7 @@ export default defineConfig({
         chunkFileNames: `[name].[hash].js`,
         entryFileNames: `[name].[hash].js`,
       }
-    }
+    },
+    publicPath: '/snu/'
   },
 });
