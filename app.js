4bee999523e5a9e445d0e0bb505c592818d45916
KIM JOONG IL <zhenos@hotmail.com>
2023-04-17 13:03:13 +0900

    merge fix

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (req, res) => {
   // res.send('Hello World!');
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log('http://localhost:3000');
+  
 });
