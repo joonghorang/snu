03bb47a20cd3b47ea9958b58d1e433670d4ea26d
KIM JOONG IL <zhenos@hotmail.com>
2023-05-15 08:35:45 +0900

    기본 챗gpt 완성

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const express = require('express');
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
-const path = require('path');
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!');
   res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
+app.post('/api/chat', async (req, res) => {
+  const prompt = req.body.prompt;
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}]
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': ``
+        }
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
-  
+  console.log(PORT);
 });
--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import axios from 'axios';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+window.onload = function () {
+  document.getElementById('chatForm').onsubmit = function (e) {
+    e.preventDefault();
 
-setupCounter(document.querySelector('#counter'))
+    const prompt = document.getElementById('prompt').value;
+    const responseElement = document.getElementById('response');
+
+    axios.post('http://localhost:3000/api/chat', { prompt: prompt })
+      .then(function (response) {
+        responseElement.textContent = response.data.choices[0].message.content;
+      })
+      .catch(function (error) {
+        console.error('Error:', error);
+        responseElement.textContent = 'Error: ' + error.message;
+      });
+  };
+};
+
+
